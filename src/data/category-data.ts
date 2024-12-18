ce2dc97de17a6b4e149ced848a1bc9eab3564928
Khaled El Hamzi <contact@elhamzi.me>
2024-12-18 21:18:51 +0100

    create a dummy data.

---
+++ src/data/category-data.ts
@@ -0,0 +1,10 @@
+import { CategoryType } from "../types/CategoryType";
+
+export const categories: CategoryType[] = [
+    { id: 1, name: "Electronics" },
+    { id: 2, name: "Home Appliances" },
+    { id: 3, name: "Accessories" },
+    { id: 4, name: "Wearables" },
+    { id: 5, name: "Gaming" },
+    { id: 6, name: "Furniture" }
+];
---
+++ src/data/product-data.ts
@@ -0,0 +1,14 @@
+import { ProductType } from "../types/ProductType";
+
+export const products: ProductType[] = [
+    { id: 0, name: "Laptop", categoryId: 1 },
+    { id: 1, name: "Smartphone", categoryId: 1 },
+    { id: 2, name: "Tablet", categoryId: 1 },
+    { id: 3, name: "Smartwatch", categoryId: 4 },
+    { id: 4, name: "Gaming Console", categoryId: 5 },
+    { id: 5, name: "Refrigerator", categoryId: 2 },
+    { id: 6, name: "Microwave", categoryId: 2 },
+    { id: 7, name: "Headphones", categoryId: 3 },
+    { id: 8, name: "Office Chair", categoryId: 6 },
+    { id: 9, name: "Gaming Chair", categoryId: 6 }
+];
