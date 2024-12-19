4fcd93e963fa89d003e0988b5b1091f16ce4c703
Khaled El Hamzi <contact@elhamzi.me>
2024-12-19 17:52:18 +0100

    feat: implement a loading indicator

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -7,20 +7,25 @@ import { CanceledError } from 'axios';
 const UserList = () => {
 	const [users, setUsers] = useState<UserType[]>([]);
 	const [error, setError] = useState<string | null>(null);
+	const [isLoading, setLoading] = useState<boolean>(false);
 
 	useEffect(() => {
 		// Create AbortController instance
 		const controller = new AbortController();
 
+		setLoading(true);
 		getUsers(controller.signal)
-			.then(({ data }) => setUsers(data))
+			.then(({ data }) => {
+				setUsers(data);
+			})
 			.catch((err) => {
 				if (err instanceof CanceledError) return;
 				else {
 					setError(`Failed to fetch users: ${err}`);
 					toast.error('Error fetching users!');
 				}
-			});
+			})
+			.finally(() => setLoading(false));
 
 		// Cleanup: Abort ongoing requests when component unmounts
 		return () => controller.abort();
@@ -29,14 +34,20 @@ const UserList = () => {
 	return (
 		<div className="mt-3">
 			<h3>User List</h3>
-			{error && <p className="text-danger">{error}</p>} {/* Display error if any */}
-			<ul className="px-0">
-				{users.map((user: UserType) => (
-					<li className="list-unstyled" key={user.id}>
-						{user.name}
-					</li> // Render each user (update based on your data structure)
-				))}
-			</ul>
+			{error && <p className="text-danger">{error}</p>}
+
+			{isLoading && <div className="spinner-border"></div>}
+
+			{!isLoading && !error && (
+				<ul className="px-0">
+					{users.map((user: UserType) => (
+						<li className="list-unstyled" key={user.id}>
+							{user.name}
+						</li> // Render each user (update based on your data structure)
+					))}
+				</ul>
+			)}
+
 			<ToastContainer />
 		</div>
 	);
